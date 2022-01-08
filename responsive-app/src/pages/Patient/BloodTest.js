01a99eeee0cc023907c525d7e370721db2061c8d
olahjg <k75681146744@ktch.hu>
2022-01-08 20:43:09 +0100

    responsive dates

--- responsive-app/src/pages/Patient/BloodTest.js
+++ responsive-app/src/pages/Patient/BloodTest.js
@@ -4,6 +4,8 @@ import API from "../../utils/API";
 import Button from "../../components/Button";
 import PatientBloodTestModal from "../../components/PatientBloodTestModal";
 
+import "./BloodTest.css";
+
 const BloodTest = () => {
   useEffect(() => {
     API.get("/patientBloodTestDates").then((result) =>
@@ -23,17 +25,21 @@ const BloodTest = () => {
     <div className="patient-page-container">
       <div className="patient-content-container">
         <h1>Vérvételeid</h1>
-        {bloodTestsDate.map((e) => {
-          return (
-            <Button
-              key={e.blood_tests_taken_date}
-              onClick={() => {
-                renderBloodTestModal(e.blood_tests_taken_date.split("T")[0]);
-              }}
-              text={e.blood_tests_taken_date.split("T")[0]}
-            />
-          );
-        })}
+        <div className="patient-content-date-container">
+            {bloodTestsDate.map((e) => {
+              return (
+                <Button
+                  key={e.blood_tests_taken_date}
+                  onClick={() => {
+                    renderBloodTestModal(
+                      e.blood_tests_taken_date.split("T")[0]
+                    );
+                  }}
+                  text={e.blood_tests_taken_date.split("T")[0]}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
