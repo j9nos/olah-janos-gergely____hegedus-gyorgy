786d8409d4e10db5f187faf81ba878082466b81e
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-22 12:38:32 +0100

    home page kesz

--- desktop-app/src/pages/Home.js
+++ desktop-app/src/pages/Home.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import "./Home.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="home-main-container">
+        <div className="home-top-container">
+          <h1 className="title">Udvozoljuk a Medicloud Admin feluleten</h1>
+          <p>Itt kezelheti a doktorok es a pacienkse adatait egyarant</p>
+          <p>
+            Lehetosege van a paciensek es a doktorok adatainak
+            modositasara,torlesere, es uj adata felvetelere.
+          </p>
+        </div>
+        <div className="home-bot-container">
+          <button
+            className="homeBtn"
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "http://localhost:3000/patients";
+            }}
+          >
+            Paciensek
+          </button>
+          <button
+            className="homeBtn"
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "";
+            }}
+          >
+            Weboldal
+          </button>
+          <button
+            className="homeBtn"
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.location.href = "http://localhost:3000/doctors";
+            }}
+          >
+            Doktorok
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
