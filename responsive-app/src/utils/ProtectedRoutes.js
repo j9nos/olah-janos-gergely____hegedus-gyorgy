04295ada9998f39b948959e78b7e455df7bb643f
Olah Janos Gergely <71344849+j9nos@users.noreply.github.com>
2022-03-20 15:56:46 +0100

    Delete ProtectedRoutes.js

--- responsive-app/src/utils/ProtectedRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { getToken } from "./authorization";
-
-export function LoggedInRoute() {
-  return getToken() ? <Outlet /> : <Navigate to="/login" />;
-}
-export function LoggedOutRoute() {
-  return !getToken() ? <Outlet /> : <Navigate to="/" />;
-}
